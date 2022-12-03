84416a940579b1397055d953cf60946d684db9d1
ch2isk4kos <ch2isk4kos@gmail.com>
2022-12-02 20:55:05 -0500

    test gsap with timeline sequence

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log({ gsap });
+
+const tl = gsap.timeline();
+
+tl.to(".m", { duration: 2, x: 100 })
+  .to(".a", { duration: 1, y: 200 })
+  .to(".i", { duration: 3, rotation: 360 })
+  .to(".n", { duration: 3, rotation: 360 });
